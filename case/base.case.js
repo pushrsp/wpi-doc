e0aec74b989829b51831e898ae29167e64e0106c
pushrsp@gmail.com <pushrsp@gmail.com>
2021-12-22 23:37:48 +0900

    UPDATE: 정규식

--- case/base.case.js
+++ case/base.case.js
@@ -1,24 +1,13 @@
 /**
- * @wpiVersion 0.0.1
- * @wpiName 테스트입니다.
- * @wpiDescription 테스트 디스크립션
  * @wpiRoute api/test [post]
- * @wpiBody {string} name 이름
- * @wpiBody {string} nickname 별명
+ * @wpiDescription 테스트입니다.
+ * @wpiBody {string} username 아이디
  * @wpiBody {string} password 비밀번호
+ * @wpiBody {string} nickname 닉네임
  * @wpiSuccess {string} message 메세지
  * @wpiSuccess {int} statusCode 상태코드
  * @wpiSuccess {object} data 데이터
- */
-
-/**
- * @wpiVersion 0.0.2
- * @wpiName 두번제 테스트
- * @wpiRoute api/test2 [get]
- * @wpiBody {string} name 이름
- * @wpiBody {string} nickname 별명
- * @wpiBody {string} password 비밀번호
- * @wpiSuccess {string} message 메세지
- * @wpiSuccess {int} statusCode 상태코드
- * @wpiSuccess {object} data 데이터
+ * @wpiFail {string} message 메세지
+ * @wpiFail {int} statusCode 상태코드
+ * @wpiFail {string} error 에러
  */
--- test/basic.test.js
+++ test/basic.test.js
@@ -1,4 +1,6 @@
 const Parser = require("../lib/core/parser");
+
+const { getValue } = require("../lib/utils/register");
 const { DO_NOT_TOUCH } = require("../lib/utils/factory");
 
 const wpiParser = new Parser();
@@ -11,62 +13,37 @@ describe("basic test", function () {
 
   it("batcher should read all .js file and return array of string", function () {
     const data = wpiParser.batcher([`${DO_NOT_TOUCH.PWD}/case/base.case.js`]);
-    expect(data).toStrictEqual([
-      "/**\n" +
-        " * @wpiVersion 0.0.1\n" +
-        " * @wpiName 테스트입니다.\n" +
-        " * @wpiDescription 테스트 디스크립션\n" +
-        " * @wpiRoute api/test [post]\n" +
-        " * @wpiBody {string} name 이름\n" +
-        " * @wpiBody {string} nickname 별명\n" +
-        " * @wpiBody {string} password 비밀번호\n" +
-        " * @wpiSuccess {string} message 메세지\n" +
-        " * @wpiSuccess {int} statusCode 상태코드\n" +
-        " * @wpiSuccess {object} data 데이터\n" +
-        " */\n" +
-        "\n" +
-        "/**\n" +
-        " * @wpiVersion 0.0.2\n" +
-        " * @wpiName 두번제 테스트\n" +
-        " * @wpiRoute api/test2 [get]\n" +
-        " * @wpiBody {string} name 이름\n" +
-        " * @wpiBody {string} nickname 별명\n" +
-        " * @wpiBody {string} password 비밀번호\n" +
-        " * @wpiSuccess {string} message 메세지\n" +
-        " * @wpiSuccess {int} statusCode 상태코드\n" +
-        " * @wpiSuccess {object} data 데이터\n" +
-        " */\n",
-    ]);
+
+    expect(data.length).toBeGreaterThan(0);
   });
 
   it("batcher should not search any keyword except comment", async function () {
     const paths = await wpiParser.collector();
     const batcher = wpiParser.batcher(paths);
     expect(batcher.length).toBeGreaterThan(0);
+    expect(getValue("address").length).toBeGreaterThan(0);
   });
 
-  it("should return formatted return value", function () {
-    const result = wpiParser.parse();
-    expect(result).toStrictEqual({
-      version: "0.0.1",
-      name: "테스트입니다.",
-      description: "테스트 디스크립션",
-      result: [
-        {
-          route: "api/test",
-          method: "post",
-          body: [
-            { key: "name", type: "string", explain: "이름" },
-            { key: "nickname", type: "string", explain: "별명" },
-            { key: "password", type: "string", explain: "비밀번호" },
-          ],
-          success: [
-            { key: "message", type: "string", explain: "메세지" },
-            { key: "statusCode", type: "int", explain: "상태코드" },
-            { key: "data", type: "object", explain: "데이터" },
-          ],
-        },
-      ],
-    });
+  it("should return formatted return value", async function () {
+    const paths = await wpiParser.collector();
+    const batcher = wpiParser.batcher([`${DO_NOT_TOUCH.PWD}/case/base.case.js`]);
+    const result = wpiParser.parse(batcher);
+    expect(result).toStrictEqual([
+      {
+        route: "api/test",
+        method: "POST",
+        description: "테스트 디스크립션",
+        body: [
+          { key: "name", type: "string", explain: "이름" },
+          { key: "nickname", type: "string", explain: "별명" },
+          { key: "password", type: "string", explain: "비밀번호" },
+        ],
+        success: [
+          { key: "message", type: "string", explain: "메세지" },
+          { key: "statusCode", type: "int", explain: "상태코드" },
+          { key: "data", type: "object", explain: "데이터" },
+        ],
+      },
+    ]);
   });
 });
